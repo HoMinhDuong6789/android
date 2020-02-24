7448e2138439214d27158f2dc3512f16ff80ea36
Mitt <mitt@enclave.vn>
2020-02-24 16:02:52 +0700

    #6.2 - Fragment (Replace)

--- fragment/app/src/main/java/com/example/fragment/MainActivity.java
+++ fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -11,24 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initFragment();
         showFirstFragment();
+        //initFragment();
+        //showFirstFragment();
     }
 
-    private void showFirstFragment() {
+    /*private void showFirstFragment() {
         getSupportFragmentManager().beginTransaction().show(firstFragment).hide(secondFragment).commit();
     }
-
     private void showSecondFragment() {
         getSupportFragmentManager().beginTransaction().show(secondFragment).hide(firstFragment).addToBackStack(null).commit();
     }
+    */
 
     private void initFragment() {
         firstFragment = new FirstFragment();
         firstFragment.setListener(new FirstFragment.OnClickListener() {
             @Override
             public void clickButton() {
-                showSecondFragment();
+            //   showSecondFragment();
             }
         });
         secondFragment = new SecondFragment();
@@ -36,5 +37,35 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.ln_main, firstFragment, FirstFragment.class.getName())
                 .add(R.id.ln_main, secondFragment, SecondFragment.class.getName())
                 .commit();
+    }
+
+
+
+    private void showFirstFragment(){
+        if(firstFragment ==null){
+            firstFragment = new FirstFragment();
+            firstFragment.setListener(new FirstFragment.OnClickListener() {
+                @Override
+                public void clickButton() {
+                    showSecondFragment();
+                }
+            });
+
+        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.ln_main, firstFragment, FirstFragment.class.getName()).commit();
+
+
+    }
+
+    private void showSecondFragment(){
+        if(secondFragment ==null){
+            secondFragment = new SecondFragment();
+        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.ln_main, secondFragment, SecondFragment.class.getName())
+                .addToBackStack(null).commit();
+
+
     }
 }
